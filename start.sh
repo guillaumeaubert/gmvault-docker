e3f4af086f434a419cffeb77e249faecb4e53650
Ekhozie <ekhozie@users.noreply.github.com>
2020-05-15 15:56:30 +0200

    Fix missing export so that GMVAULT_SEND_REPORTS_TO is visible in inner scripts

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ if [ "$GMVAULT_OPTIONS" != "" ]; then
 fi
 
 # Ensure there's an address to send reports to.
-GMVAULT_SEND_REPORTS_TO=${GMVAULT_SEND_REPORTS_TO:="$GMVAULT_EMAIL_ADDRESS"}
+export GMVAULT_SEND_REPORTS_TO=${GMVAULT_SEND_REPORTS_TO:="$GMVAULT_EMAIL_ADDRESS"}
 echo "Sending email reports to $GMVAULT_SEND_REPORTS_TO."
 
 # Adjust timezone.
